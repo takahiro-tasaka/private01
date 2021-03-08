9f6271e7618901ba5f1fe2fae6a79abd88fec35f
takahiro-tasaka <hotar3ka.t.t@gmail.com>
2021-03-08 14:44:41 +0900

    ブログ投稿＆編集機能実装

---
+++ blog/resources/views/show.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+    
+        <link href="https:/fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <div class='post'>
+            <h2 class='title'>{{ $post->title }}</h2>
+            <p class='body'>{{ $post->body }}</p>
+            <p class='updated_at'>{{ $post->updated_at }}</p>
+        </div>
+        <div class="back">[<a href="/">戻る</a>]</div>
+    </body>
+</html>
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,7 +13,9 @@
 
 use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
+Route::get('/posts/{post}', 'PostController@show');
+Route::post('/posts', 'PostController@store');
